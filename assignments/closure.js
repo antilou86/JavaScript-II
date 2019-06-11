4af511c845f5a3576b7068c06badcb18184b3e79
Lou <antilou86@gmail.com>
2019-06-11 17:31:57 -0400

    Technically completed, moving on to stretch problems

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,10 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const closure = 'Hey, look, it worked!';
+let pleaseWork = () => {
+  return closure;
+}
+console.log(pleaseWork())
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
